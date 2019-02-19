bd9d4a605a21a2866c9d8d343b868a1d6c071a4b
kevin viglucci <kviglucci@gmail.com>
2019-02-18 23:01:08 -0600

    add error handling of realm resolution

--- src/app/resolver/GuildResolver.ts
+++ src/app/resolver/GuildResolver.ts
@@ -1,8 +1,12 @@
-import { injectable } from 'inversify';
+import { injectable, inject } from 'inversify';
 import IDataSources from '../../interfaces/IDataSources';
+import Logger from '../logging/Logger';
 
 @injectable()
 export default class GuildResolver {
+  @inject(Logger)
+  private logger: Logger;
+
   public getDefinition() {
     return {
       Guild: {
@@ -16,8 +20,17 @@ export default class GuildResolver {
     args: any,
     ctx: { dataSources: IDataSources }
   ): Promise<any> {
-    return ctx.dataSources.realms.getRealmBySlug(guild.realm.slug, {
-      region: guild.region
-    });
+    let realm = null;
+    try {
+      realm = await ctx.dataSources.realms.getRealmBySlug(guild.realm.slug, {
+        region: guild.region
+      });
+    } catch (err) {
+      this.logger.error(
+        { err: err.extensions },
+        'Failed to resolve realm for guild.'
+      );
+    }
+    return realm;
   }
 }
